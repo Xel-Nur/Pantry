d46dfeef6c97f6908cfe20c12b4bde361db3089b
Xel-Nur <file07903@gmail.com>
2024-08-06 23:46:15 +0100

    20:51

--- code/inventory_manage/app/page.js
+++ code/inventory_manage/app/page.js
@@ -4,12 +4,74 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
+import { collection, query, getDocs, setDoc, deleteDoc, doc } from "firebase/firestore";
 
 export default function Home() {
-  const {inventory, setInventory} = useState([])
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState(false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async () => {
+    const snapshot= query(collection(firestore,'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList=[]
+    docs.forEach((doc)=>{
+      inventoryList.push({
+        name:doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+    
+  }
+  const addItem = async (item)=>{
+    const docRef=doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef,{quantity:quantity +1})
+      
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+    await updateInventory()
+  }
+
+  const removeItem = async (item)=>{
+    const docRef=doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if (quantity==1){
+        await deleteDoc(docRef)
+      }
+      else{
+        await setDoc(docRef,{quantity:quantity-1})
+      }
+    }
+    await updateInventory()
+  }
+  useEffect(()=>{
+    updateInventory()
+  }, [])
+
+  
+
   return (
     <Box>
       <Typography variant="h2">inventory management</Typography>
+      {
+        inventory.forEach((item)=>{
+          return(
+            <>{item.name}
+              {item.count}
+            </>
+          )
+        })
+      }
     </Box>
   );
 }
